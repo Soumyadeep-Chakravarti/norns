3adc7d2c1f7a8063a4d8254f08f6094311448697
Soumyadeep Chakravrti <soumyadeepsai1@gmail.com>
2026-09-15 13:41:39 +0530

    refactor: establish Norns workspace

---
+++ .nix/flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Norns development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              rustc
+              cargo
+              rustfmt
+              clippy
+              rust-analyzer
+
+              pkg-config
+              openssl
+
+              just
+            ];
+
+            RUST_BACKTRACE = "1";
+
+            shellHook = ''
+              echo "ᚾ Norns"
+              echo "  $(rustc --version)"
+            '';
+          };
+        }
+      );
+    };
+}
